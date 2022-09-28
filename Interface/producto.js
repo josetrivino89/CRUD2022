82f54f4e482ffcea27d213b2e6e736cb1d4fa7da
josetrivino89 <josetrivino89@gmail.com>
2022-09-27 21:59:26 -0300

    se creo la funcion para borrar los productos

--- Interface/producto.js
+++ Interface/producto.js
@@ -1,42 +1,42 @@
-export class Producto {
-    constructor (codigo,producto,precio,descripcion,URL){
-    this.codigo = codigo;
-    this.producto = producto ;
-    this.precio = precio ;
-    this.descripcion = descripcion ;
-    this.URL = URL ;
+// export class Producto {
+//     constructor (codigo,producto,precio,descripcion,URL){
+//     this.codigo = codigo;
+//     this.producto = producto ;
+//     this.precio = precio ;
+//     this.descripcion = descripcion ;
+//     this.URL = URL ;
     
- }
-    get getCodigo(){
-        return this.codigo
-    }
-    get getProducto(){
-        return this.producto;
-    }
-    get getPrecio(){
-        return this.precio;
-    }
-    get getDescripcion(){
-        return this.descripcion;
-    }
-    get getURL(){
-        return this.URL;
-    }
+//  }
+//     get getCodigo(){
+//         return this.codigo
+//     }
+//     get getProducto(){
+//         return this.producto;
+//     }
+//     get getPrecio(){
+//         return this.precio;
+//     }
+//     get getDescripcion(){
+//         return this.descripcion;
+//     }
+//     get getURL(){
+//         return this.URL;
+//     }
     
 
-    set setCodigo(codigo){
-        this.codigo=codigo
-    }
-    set setProducto(producto){
-        this.producto=producto 
-    }
-    set setPrecio(precio){
-        this.precio=precio
-    }
-    set setDescripcion(descripcion){
-        this.descripcion=descripcion
-    }
-    set setURL(URL){
-        this.URL= URL
-    }
-}
+//     set setCodigo(codigo){
+//         this.codigo=codigo
+//     }
+//     set setProducto(producto){
+//         this.producto=producto 
+//     }
+//     set setPrecio(precio){
+//         this.precio=precio
+//     }
+//     set setDescripcion(descripcion){
+//         this.descripcion=descripcion
+//     }
+//     set setURL(URL){
+//         this.URL= URL
+//     }
+// }
--- js/admin.js
+++ js/admin.js
@@ -1,9 +1,20 @@
+class Producto {
+    constructor (codigo,producto,precio,descripcion,URL){
+    this.codigo = codigo;
+    this.producto = producto ;
+    this.precio = precio ;
+    this.descripcion = descripcion ;
+    this.URL = URL ;    
+ }
+}
+
 console.log(`desde admin.js`);
 /* importo la clase producto desde la interface*/
- import {Producto} from "../Interface/producto.js";
+//* import {Producto} from "../Interface/producto.js";
+
+
 
  /*obtengo los inputs de los arregos*/
-
  let inputCodigo = document.getElementById(`inputCodigo`);
  let inputProducto = document.getElementById(`inputProducto`);
  let inputPrecio = document.getElementById(`inputPrecio`);
@@ -30,6 +41,7 @@ console.log(`desde admin.js`);
    console.log(arrayProducto);
    //*mando los datos a local storage usando metoso stringify
    localStorage.setItem(`Listado de Productos`,JSON.stringify(arrayProducto))
+   //*utilizo window.location.reload para actualizar la pagina
    window.location.reload()
    //*reseteo formulario para que puedan completar siguiente producto
    form.reset()
@@ -39,12 +51,12 @@ console.log(`desde admin.js`);
  form.addEventListener(`submit`,handleSubmit);
 
  //*Traigo el tbody del html:
-
  let tBodyListaProducto = document.getElementById("bodyListaProductosAdmin")
  console.log(tBodyListaProducto);
+
  //*recorro el arrayProducto para ir agregando los nuevos productos:
  arrayProducto.forEach(producto => {
-    tBodyListaProducto.innerHTML +=
+    return tBodyListaProducto.innerHTML +=
     `<tr>
         <th class="text-center">${producto.codigo}</th>
         <th class="text-center">${producto.producto}</th>
@@ -52,9 +64,28 @@ console.log(`desde admin.js`);
         <th class="text-center">u$s:${producto.precio}</th>
         <th class="text-center">${producto.URL}</th>
         <th class="text-center">
-            <button class="btn btn-primary text-center">Borrar</button>
+            <button class="btn btn-primary text-center" onclick="borrarProducto(${producto.codigo})">Borrar</button>
             <button class="btn btn-danger text-center">Editar</button>
             
         </th>
     </tr>`
  });
+ //*tengo q pasarle si o si al evento onclick el parametro para que la funcion se asocie al codigo, solo asi sabra a que codigo hace referencia cada producto*//
+
+//*creamos una funcion para borrar un producto:*//
+ const borrarProducto = (codigo) => {
+    console.log(codigo);
+    //*para borrar un producto debo filtar el array y devolverlo sin el producto, para ello necesito el codigo para vincularlo, para eso usamos el metodo filter:
+    const arrayFiltrado = arrayProducto.filter(producto =>{
+        //*utilizamos el metodo toString ya que el array esta en formato string , el metodo tostring transforma un numero en string y lo vuelve comparable:
+        return producto.codigo !== codigo.toString()
+    })
+    console.log(arrayFiltrado);
+    //* ahora cambio mi arrayproducto por el nuevo arrayfiltrado, es decir lo reemplazo:*//
+    arrayProducto = arrayFiltrado;
+    //* ahora necesito enviar el nuevo array a JSON:*//
+    localStorage.setItem(`Listado de Productos`,JSON.stringify(arrayProducto))
+    //*actualizo la pagina para cargar los cambios
+    window.location.reload()
+
+ }
